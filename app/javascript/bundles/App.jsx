18ac626442694017d97a87eb3503dac79e106014
Stommy123 <Stommy49@Yahoo.com>
2019-01-24 20:50:00 -0500

    fixed create function

--- app/javascript/bundles/App.jsx
+++ app/javascript/bundles/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import PlaceForm from './components/Place/PlaceForm.jsx';
 import PlaceMap from './components/Place/PlaceMap.jsx';
 import { headers } from './utilities/utils';
 
-const App = _ => {
-  const createPlace = async p => await axios.post(
-    '/places',
-    { place: { ...p } },
-    { headers: headers }
-  )
+class App extends Component {
+  createPlace = async p => {
+    console.log(p)
+    await axios.post(
+      '/places',
+      { place: { ...p } },
+      { headers: headers }
+    )
+  }
+  render() {
     return (
       <div id='place-container'>
         <PlaceMap />
         <PlaceForm createPlace={this.createPlace} />
       </div>
     )
+  }
 }
+
 
 export default App
